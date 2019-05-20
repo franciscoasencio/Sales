502466b30755a298695092ca8cabb871695fb7b4
franciscoasencio <francisco.asencio@gmail.com>
2019-05-20 18:48:04 -0400

    Justo antes de pintar la Lista de productos

---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Sales.Common.Models;
+using System.Collections.ObjectModel;
+
+namespace Sales.ViewModels
+{
+
+    public class ProductsViewModel
+    {
+
+        public ObservableCollection<Product> Products { get; set; }
+
+    }
+}

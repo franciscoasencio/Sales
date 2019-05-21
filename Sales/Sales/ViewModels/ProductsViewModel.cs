d167284d294b4e943b18192510ad8ae40cef1c05
franciscoasencio <francisco.asencio@gmail.com>
2019-05-21 15:18:37 -0400

    Corriendo pero error "Not Found" de Somee

--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -3,7 +3,9 @@
     using Sales.Common.Models;
     using Sales.Services;
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using Xamarin.Forms;
 
     public class ProductsViewModel : BaseViewModel
     {
@@ -25,7 +27,15 @@
 
         private async void LoadProducts()
         {
-            var response = await this.apiService.GetList<Product>("http://fasencio.somee.com/Sales/API", "/API","/Products"); 
+            var response = await this.apiService.GetList<Product>("http://fasencio.somee.com/Sales/API", "/API","/Products");
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                return;
+            }
+            var list = (List<Product>)response.Result;
+            this.Products = new ObservableCollection<Product>(list);
+
         }
     } 
 }

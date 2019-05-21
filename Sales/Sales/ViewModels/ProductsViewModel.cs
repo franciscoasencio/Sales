c6e326d4e62698b569423c09de7a9873ff933f78
franciscoasencio <francisco.asencio@gmail.com>
2019-05-21 14:51:49 -0400

    Completado la carga del apiService Products

--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -1,6 +1,8 @@
 ﻿namespace Sales.ViewModels
 {
     using Sales.Common.Models;
+    using Sales.Services;
+    using System;
     using System.Collections.ObjectModel;
 
     public class ProductsViewModel : BaseViewModel
@@ -13,6 +15,17 @@
         {
             get { return this.products; }
             set { this.SetValue(ref this.products, value); }
+        }
+
+        public ProductsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            var response = await this.apiService.GetList<Product>("http://fasencio.somee.com/Sales/API", "/API","/Products"); 
         }
     } 
 }

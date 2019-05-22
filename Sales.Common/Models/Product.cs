b541e6424b1c9940edad019e62748d571de99ff9
franciscoasencio <francisco.asencio@gmail.com>
2019-05-22 08:54:58 -0400

    Avances en display list Mobile

--- Sales.Common/Models/Product.cs
+++ Sales.Common/Models/Product.cs
@@ -16,7 +16,11 @@
         public bool IsAvailable { get; set; }
 
         public DateTime PublishOn { get; set; }
-        
+        public override string ToString()
+        {
+            return this.Description;
+        }
+
     }
 
 }
